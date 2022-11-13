70e27c0197b3c4e9a920c3635091300ef701d6b9
M0Rek <morek.090@gmail.com>
2022-11-13 19:34:36 +0100

    Add validation for email and loggging in.

--- javascript/validation.js
+++ javascript/validation.js
@@ -10,18 +10,19 @@
     retyped.addEventListener("input", () => {
         if (password.value !== retyped.value) {
             retyped.reportValidity();
-        }})
+        }
+    })
 
     regForm.addEventListener('submit', event => {
 
         const htmlValidation = regForm.checkValidity()
         const customValidation = IsValidRegistration()
 
-       if (!htmlValidation || !customValidation) {
-                event.preventDefault()
-                event.stopPropagation()
-            }
-        }, false)
+        if (!htmlValidation || !customValidation) {
+            event.preventDefault()
+            event.stopPropagation()
+        }
+    }, false)
 
 
     loginForm.addEventListener('submit', event => {
@@ -39,20 +40,25 @@
 
 function IsValidRegistration() {
     const login = document.getElementById("register-username")
+    const email = document.getElementById("register-email")
     const password = document.getElementById("register-password")
     const retyped = document.getElementById("register-password-retyped")
 
     let result = true
 
-    if(login.value.match(/^[A-Za-z0-9]{8,16}$/g) == null) {
+    if (login.value.trim().match(/^[A-Za-z0-9]{8,16}$/g) == null) {
         result = false
     }
 
-    if(password.value.match(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{8,20}$/g) == null) {
+    if (password.value.match(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{8,20}$/g) == null) {
         result = false
     }
 
-    if(password.value !== retyped.value) {
+    if (password.value !== retyped.value) {
+        result = false
+    }
+
+    if (email.value == null || email.value.trim() === "") {
         result = false
     }
 
@@ -66,11 +72,21 @@ function IsValidLogin() {
 
     let result = true
 
-    if(login.value == null) {
+
+    //Formalna poprawność w trakcie logowania składa się tylko z sprawdzenia
+    // czy pola nie są puste.
+    //
+    // Zakładam, że zasady tworzenia loginów i haseł mogą zmieniać się w czasie
+    // i nie powinno się blokować możliwości logowania kontom sprzed zmiany zasad.
+    //
+    // Gdyby nie zważać na to, zostosowałbym tę samą weryfikację pól jaka następuje
+    // przy próbie rejestracji.
+
+    if (login.value == null || login.value.trim() === "") {
         result = false
     }
 
-    if(password.value == null) {
+    if (password.value == null || password.value === "") {
         result = false
     }
 
