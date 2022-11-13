528c26268978193035fee6d69329feea215aceeb
M0Rek <morek.090@gmail.com>
2022-11-13 18:23:01 +0100

    Add registration ok page.

---
+++ rejestracja-ok.php
@@ -0,0 +1,21 @@
+<?php
+require("include/function.php");
+session_start();
+
+echo head("Pomyślna rejestracja");
+
+
+print_r($_SESSION["user-data"]);
+
+?>
+
+
+
+<?php
+
+echo footer();
+
+
+
+
+?>
