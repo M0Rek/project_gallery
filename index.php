3f45017525108576ec06abfdec04fb6ff62a3233
M0Rek <morek.090@gmail.com>
2022-11-14 18:24:11 +0100

    Add gallery, sorting and pagination.

--- index.php
+++ index.php
@@ -1,7 +1,115 @@
 <?php
 
 require("include/function.php");
+require("include/database.php");
+
+//Wartości nie są bezpośrednio wprowadzone do kwerendy, co zapobiega sql injection
+if(isset($_GET["sort"])) {
+    switch ($_GET["sort"]) {
+        case "data":
+            $sort = "data";
+            break;
+        case "tworca":
+            $sort = "tworca";
+            break;
+        default:
+            $sort = "tytul";
+            break;
+    }
+}
+else {
+    $sort = "tytul";
+}
+
+if (isset($_GET["asc"]) && $_GET["asc"] == "false")
+    $asc = "DESC";
+else $asc = "ASC";
+
+$conn = connectToDB();
+$stmt = $conn->prepare("SELECT count(*) as count FROM albumy INNER JOIN 
+    (SELECT id,opis,id_albumu,min(data),zaakceptowane FROM zdjecia WHERE zaakceptowane = 1 GROUP BY id_albumu) as zaakceptowane on zaakceptowane.id_albumu = albumy.id
+        LEFT JOIN uzytkownicy ON id_uzytkownika = uzytkownicy.id");
+$stmt->execute();
+$result = $stmt->get_result();
+
+$itemsCount = $result->fetch_assoc()["count"];
+$itemsPerPage = 2;
+$pageCount = ceil($result->num_rows / $itemsPerPage);
+
+$stmt = $conn->prepare("SELECT albumy.id as id, zaakceptowane.id as zdjecie,tytul, date(data) as data ,uzytkownicy.login as tworca FROM albumy INNER JOIN 
+    (SELECT id,opis,id_albumu,min(data),zaakceptowane FROM zdjecia WHERE zaakceptowane = 1 GROUP BY id_albumu) as zaakceptowane on zaakceptowane.id_albumu = albumy.id
+        LEFT JOIN uzytkownicy ON id_uzytkownika = uzytkownicy.id ORDER BY " . $sort . " " . $asc);
+$stmt->execute();
+$result = $stmt->get_result();
+
+while ($row = $result->fetch_assoc()) {
+    $albums[] = $row;
+}
 
 echo head("Strona główna");
 
+?>
+
+
+<div class="container-md mt-5">
+
+    <div class="row m-3">
+        <div class="col-md-4">
+            <label for="album-sort">
+                Sortowanie:
+            </label>
+            <select onchange="sortAlbums()" id="album-sort" class="form-select"
+                    aria-label="Sortowanie">
+                <option value="tytul">Nazwa</option>
+                <option value="data">Data</option>
+                <option value="tworca">Twórca</option>
+            </select>
+            <div class="form-check form-switch pt-2">
+                <input class="form-check-input" checked onchange="sortAlbums()" type="checkbox" id="album-asc">
+                <label class="form-check-label" for="album-asc">Sortuj rosnąco</label>
+            </div>
+        </div>
+        <script>
+
+            <?php
+            if (isset($_GET["sort"])) {
+                echo 'document.getElementById("album-sort").value = "' . $_GET["sort"] . '";';
+            }
+
+            if (isset($_GET["asc"])) {
+                echo 'document.getElementById("album-asc").checked = ("' . $_GET["asc"] . '" == "false") ? false : true ;';
+            }
+
+            ?>
+
+            function sortAlbums() {
+                const sort = document.getElementById("album-sort").value;
+                const desc = document.getElementById("album-asc").checked;
+
+                document.location.href = "index.php?sort=" + sort + "&asc=" + desc;
+            }
+        </script>
+    </div>
+
+
+    <div class="row d-flex justify-content-center g-3">
+
+        <?php
+        foreach ($albums as $album) {
+            echo '<div class="d-flex w-auto justify-content-center">
+            <img alt="'.$album["tytul"].'" class="album-thumbnail" onclick="window.location.href=\'album.php?id=' . $album["id"] . '\'" data-bs-toggle="tooltip" data-bs-html="true" data-bs-placement="bottom" 
+    data-bs-title="Tytuł: ' . $album["tytul"] . '<br> Utworzono: ' . $album["data"] . '<br> Twórca: ' . $album["tworca"] . '" 
+    src="photo/' . $album["id"] . '/' . $album["zdjecie"] . '.jpg"></div>';
+        }
+        ?>
+    </div>
+    <div class="row m-3">
+        <?php echo pagination(5, 10, "") ?>
+    </div>
+</div>
+
+
+<?php
+
 echo footer();
+?>
