769c993198c069d1819e1caa60362c774568c5b6
M0Rek <morek.090@gmail.com>
2022-11-14 23:45:14 +0100

    Simplify if statement in rejestracja.php.

--- rejestracja.php
+++ rejestracja.php
@@ -3,104 +3,101 @@
 require("include/function.php");
 require("include/database.php");
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if ($_POST['T'] == 'register') {
-        session_unset(); // Rozważyć zastąpienie unsetem indywidualnych pól sesji
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && $_POST['T'] == 'register') {
+    session_unset(); // Rozważyć zastąpienie unsetem indywidualnych pól sesji
 
-        function returnIfErrors($login, $password, $retyped, $email)
-        {
-            if (isset($_SESSION["registration-error"])) {
-                $recovery["login"] = $login;
-                $recovery["password"] = $password;
-                $recovery["retyped"] = $retyped;
-                $recovery["email"] = $email;
+    function returnIfErrors($login, $password, $retyped, $email)
+    {
+        if (isset($_SESSION["registration-error"])) {
+            $recovery["login"] = $login;
+            $recovery["password"] = $password;
+            $recovery["retyped"] = $retyped;
+            $recovery["email"] = $email;
 
-                $_SESSION["registration-recovery"] = $recovery;
-                header("Location: logrej.php");
-                exit();
-            }
+            $_SESSION["registration-recovery"] = $recovery;
+            header("Location: logrej.php");
+            exit();
         }
+    }
 
-        $login = trim($_POST["username"]);
-        $password = $_POST["password"];
-        $retyped = $_POST["retyped"];
-        $email = trim($_POST["email"]);
+    $login = trim($_POST["username"]);
+    $password = $_POST["password"];
+    $retyped = $_POST["retyped"];
+    $email = trim($_POST["email"]);
 
-        $loginRegex = "/[A-Za-z0-9]{8,16}/";
-        $passwordRegex = "/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{8,20}/";
+    $loginRegex = "/[A-Za-z0-9]{8,16}/";
+    $passwordRegex = "/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{8,20}/";
 
-        if (!preg_match($loginRegex, $login)) {
-            $_SESSION["registration-error"]["invalid-username"] = true;
-        }
+    if (!preg_match($loginRegex, $login)) {
+        $_SESSION["registration-error"]["invalid-username"] = true;
+    }
 
-        if (!preg_match($passwordRegex, $password)) {
-            $_SESSION["registration-error"]["invalid-password"] = true;
-        }
+    if (!preg_match($passwordRegex, $password)) {
+        $_SESSION["registration-error"]["invalid-password"] = true;
+    }
 
-        if ($password !== $retyped) {
-            $_SESSION["registration-error"]["invalid-retyped"] = true;
-        }
+    if ($password !== $retyped) {
+        $_SESSION["registration-error"]["invalid-retyped"] = true;
+    }
 
-        if ($email == "") {
-            $_SESSION["registration-error"]["invalid-email"] = true;
-        }
+    if ($email == "") {
+        $_SESSION["registration-error"]["invalid-email"] = true;
+    }
 
-        returnIfErrors($login, $password, $retyped, $email);
+    returnIfErrors($login, $password, $retyped, $email);
 
-        $conn = connectToDB();
+    $conn = connectToDB();
 
-        if (!$conn) {
-            $_SESSION["registration-error"]["database-error"] = true;
-        } else {
+    if (!$conn) {
+        $_SESSION["registration-error"]["database-error"] = true;
+    } else {
 
-            //Pole login w bazie danych nie jest oznaczone jako unikalne według założeń, więc konieczne jest manualne
-            //przeprowadzenie sprawdzenie czy login istnieje. Użyta do tego została tranzakcja by zapewnić ten sam stan bazy danych między sprawdzeniem loginu a insertem.
-            $conn->begin_transaction();
+        //Pole login w bazie danych nie jest oznaczone jako unikalne według założeń, więc konieczne jest manualne
+        //przeprowadzenie sprawdzenie czy login istnieje. Użyta do tego została tranzakcja by zapewnić ten sam stan bazy danych między sprawdzeniem loginu a insertem.
+        $conn->begin_transaction();
 
-            try {
-                $stmt = $conn->prepare("SELECT * FROM uzytkownicy where login LIKE ?");
-                $stmt->bind_param('s', $login);
-                $stmt->execute();
-                $result = $stmt->get_result();
+        try {
+            $stmt = $conn->prepare("SELECT * FROM uzytkownicy where login LIKE ?");
+            $stmt->bind_param('s', $login);
+            $stmt->execute();
+            $result = $stmt->get_result();
 
-                if ($result->num_rows == 0) {
-                    $stmt = $conn->prepare(" INSERT INTO uzytkownicy (id,login,haslo,email,zarejestrowany,uprawnienia,aktywny)
+            if ($result->num_rows == 0) {
+                $stmt = $conn->prepare(" INSERT INTO uzytkownicy (id,login,haslo,email,zarejestrowany,uprawnienia,aktywny)
                     VALUES (default, ?, md5(?), ?, NOW(), 'użytkownik', 1)");
-                    $stmt->bind_param('sss', $login, $password, $email);
+                $stmt->bind_param('sss', $login, $password, $email);
+                if (!$stmt->execute()) {
+                    $_SESSION["registration-error"]["database-error"] = true;
+                } else {
+                    $stmt = $conn->prepare("SELECT id,login,haslo,email,zarejestrowany,uprawnienia,aktywny FROM uzytkownicy where login LIKE ?");
+                    $stmt->bind_param('s', $login);
                     if (!$stmt->execute()) {
                         $_SESSION["registration-error"]["database-error"] = true;
                     } else {
-                        $stmt = $conn->prepare("SELECT id,login,haslo,email,zarejestrowany,uprawnienia,aktywny FROM uzytkownicy where login LIKE ?");
-                        $stmt->bind_param('s', $login);
-                        if (!$stmt->execute()) {
-                            $_SESSION["registration-error"]["database-error"] = true;
+                        $result = $stmt->get_result();
+                        if ($result->num_rows == 1) {
+                            $userData = getUserData($result);
                         } else {
-                            $result = $stmt->get_result();
-                            if ($result->num_rows == 1) {
-                                $userData = getUserData($result);
-                            } else {
-                                throw new mysqli_sql_exception;
-                            }
+                            throw new mysqli_sql_exception;
                         }
-                        $conn->commit();
                     }
-                } else {
-                    $_SESSION["registration-error"]["username-exists"] = true;
+                    $conn->commit();
                 }
-            } catch (mysqli_sql_exception $e) {
-                $_SESSION["registration-error"]["database-error"] = true;
-                $conn->rollback();
+            } else {
+                $_SESSION["registration-error"]["username-exists"] = true;
             }
-
+        } catch (mysqli_sql_exception $e) {
+            $_SESSION["registration-error"]["database-error"] = true;
+            $conn->rollback();
         }
 
-        returnIfErrors($login, $password, $retyped, $email);
-
-        $_SESSION["user-data"] = $userData;
-        header("Location: rejestracja-ok.php");
-        exit();
-
     }
+
+    returnIfErrors($login, $password, $retyped, $email);
+
+    $_SESSION["user-data"] = $userData;
+    header("Location: rejestracja-ok.php");
+    exit();
 }
 header("Location: index.php");
 exit();
